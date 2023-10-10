48299bff749bd0e3cd6430329a83910ab285eeea
“Kaleab” <nutmeg.quill-0m@icloud.com>
2023-10-10 10:44:58 -0400

    added 0-putcha.c and main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+int sum_of_multiples(void);
+void print_fibonacci(void);
+void sum_even_fibonacci(void);
+void print_first_98_fibonacci(void);
+
+#endif /* MAIN_H */
