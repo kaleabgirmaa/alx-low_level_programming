457bf6b3bfe9235bcba2cfec9618028969df867a
kaleabgirmaa <nutmeg.quill-0m@icloud.com>
2023-10-10 18:40:49 -0400

    added 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,33 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to check the sign of
- *
- * Description:
- * This function prints the sign of a number (+, 0, or -).
- * If the number is greater than 0, it prints "+", and returns 1.
- * If the number is 0, it prints "0", and returns 0.
- * If the number is less than 0, it prints "-", and returns -1.
- *
- * Return: The sign of the number (1 if positive, 0 if zero, -1 if negative)
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
  */
-int print_sign(int n)
+void print_to_98(int n)
 {
-	if (n > 0)
+	int i, j;
+
+	if (n <= 98)
 	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
 	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
+
