f21e6a47fc2af4f3f6ad7549840d57b418379779
kaleabgirmaa <nutmeg.quill-0m@icloud.com>
2023-10-10 18:45:06 -0400

    add 5-sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description:
+ * This function is the entry point of the program.
+ * It calls the print_sign function with different numbers and prints the result.
+ *
+ * Return: Always 0
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,17 @@
 #include "main.h"
 
 /**
-* print_sign - prints print_sign
-*
-* Description - prints the sign of a number
-*
-* Return: 0 success
-*/
-
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Description:
+ * This function prints the sign of a number.
+ * If the number is greater than 0, it prints '+' and returns 1.
+ * If the number is 0, it prints '0' and returns 0.
+ * If the number is less than 0, it prints '-' and returns -1.
+ *
+ * Return: 1 if n is greater than 0, 0 if n is 0, -1 if n is less than 0
+ */
 int print_sign(int n)
 {
 	if (n > 0)
