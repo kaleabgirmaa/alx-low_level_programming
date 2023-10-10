7f75e2330b1f5d9425c0858e8c888ac135d94569
“Kaleab” <nutmeg.quill-0m@icloud.com>
2023-10-10 14:55:08 -0400

    added 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - the function for this file
+* Description - a function that adds two integers and returns the result
+* Return: 0 success
+*/
+
+int add(int numOne, int numTwo)
+{
+	return (numOne + numTwo);
+}
