270aee746c8a0b5fd36b8fa53bbf67955ffc56ef
kaleabgirmaa <nutmeg.quill-0m@icloud.com>
2023-10-10 19:10:15 -0400

    added 101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,24 +4,24 @@
  * main - Entry point
  *
  * Description:
- * This function lists all the natural numbers below 1024 (excluded) that are multiples of 3 or 5.
- * It calculates the sum of these numbers and prints the result.
- *
- * Return: Always 0
- */
+* This function lists all the natural numbers below 1024
+* It calculates the sum of these numbers and prints the result.
+*
+* Return: Always 0
+*/
 int main(void)
 {
     int i, sum = 0;
 
     for (i = 0; i < 1024; i++)
     {
-        if ((i % 3) == 0 || (i % 5) == 0)
-        {
-            sum += i;
-        }
+    if ((i % 3) == 0 || (i % 5) == 0)
+    {
+sum += i;
+    }
     }
 
-    printf("%d\n", sum);
+printf("%d\n", sum);
 
-    return (0);
+return (0);
 }
