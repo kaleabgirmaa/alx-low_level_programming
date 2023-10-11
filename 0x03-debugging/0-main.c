741633fe3649dc0ad696d48927ec672bd93bfc1a
kaleabgirmaa <nutmeg.quill-0m@icloud.com>
2023-10-10 20:25:24 -0400

    added 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - print function
+* Description - tests function that prints if intege
+*		 is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/* prototype for positive_or_negative */
+void positive_or_negative(int i);
+
+/* prototype for largest_number */
+int largest_number(int a, int b, int c);
+
+/* prototype for convert_day */
+int convert_day(int month, int day);
+
+/* prototype for print_remaining_days */
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
