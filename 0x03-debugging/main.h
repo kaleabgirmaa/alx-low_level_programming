d0b38cf9f855762a841bb048b00cb0de782b338a
kaleabgirmaa <nutmeg.quill-0m@icloud.com>
2023-10-12 18:51:38 -0400

    added main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/* prototype for positive_or_negative */
 void positive_or_negative(int i);
-/* prototype for largest_number */
 int largest_number(int a, int b, int c);
-/* prototype for convert_day */
-int convert_day(int month, int day);
-/* prototype for print_remaining_days */
 void print_remaining_days(int month, int day, int year);
-#endif /* MAIN_H */
+int convert_day(int month, int day);
+#endif
